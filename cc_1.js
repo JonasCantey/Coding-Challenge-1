c4f8acaba614c6a1688f81ef57be9817d0567fdc
Jonas <jonashooncantey@gmail.com>
2025-01-23 00:02:22 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,9 @@
+let employeename = 'John Snow'
+const employeeID = 898
+var isActive = true
+console.log(employeename);
+console.log(employeeID)
+console.log(isActive)
+console.log(typeof employeename)
+console.log(typeof employeeID)
+console.log(typeof isActive)
