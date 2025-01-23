22c46413793500003a8dfdfdd8fd210e4fd65ff7
Jonas <jonashooncantey@gmail.com>
2025-01-23 00:34:21 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -1,9 +1,24 @@
+console.log("Task One");
 let employeename = 'John Snow'
 const employeeID = 898
 var isActive = true
+
 console.log(employeename);
 console.log(employeeID)
 console.log(isActive)
 console.log(typeof employeename)
 console.log(typeof employeeID)
 console.log(typeof isActive)
+
+//TASK TWO
+console.log("Task Two");
+let productName = 'GamingComputer'
+const productPrice = 1100
+var isAvailable = true
+
+console.log(productName)
+console.log(productPrice)
+console.log(isAvailable)
+console.log(typeof productName)
+console.log(typeof productPrice)
+console.log(typeof isAvailable)
